6fb8b2d1fc96e6fe5dbef77b7702e151015bd174
liuwenyi <liuwenyi@baidu.com>
2017-04-30 23:15:55 +0800

    add broker manager

---
+++ src/master/broker_manager.cc
@@ -0,0 +1,53 @@
+#include "broker_manager.h"
+
+#include <stdlib.h>
+#include <iostream>
+#include <thread>
+#include <memory>
+#include <string>
+
+#include <boost/crc.hpp>
+#include <boost/cstdint.hpp>
+#include <grpc++/grpc++.h>
+#include <gflags/gflags.h>
+
+#include "common.pb.h"
+
+namespace mq {
+
+Status BrokerManager::RegisterBroker(std::string ip, int32_t port) {
+    //get hash for server
+    uint32_t hash;
+    char address[30];
+    boost::crc_32_type crc32;
+    
+    sprintf(address, "%s:%d", ip.c_str(), port);
+    crc32.process_bytes(address, strlen(address));
+    hash = crc32();
+ 
+    BrokerInfo* brokerInfo = new BrokerInfo;
+    brokerInfo->set_ip(ip);
+    brokerInfo->set_port(port);
+    //insert hash
+    _brokers[hash] = brokerInfo;
+    return s_ok;
+}
+
+Status BrokerManager::UnRegisterBroker(std::string ip, int32_t port) {
+    //find hash for server
+    uint32_t hash;
+    char address[30];
+    boost::crc_32_type crc32;
+    
+    sprintf(address, "%s:%d", ip.c_str(), port);
+    crc32.process_bytes(address, strlen(address));
+    hash = crc32();
+    if (_brokers.find(hash) == _brokers.end()) {
+        return s_notfind;
+    }
+    //delete server
+    _brokers.erase(hash);
+    return s_ok;
+}
+
+}
---
+++ src/master/broker_manager.h
@@ -0,0 +1,26 @@
+#ifndef _BROKER_MANAGER_H
+#define _BROKER_MANAGER_H
+
+#include <iostream>
+#include <thread>
+#include <memory>
+#include <string>
+
+#include <grpc++/grpc++.h>
+
+#include <gflags/gflags.h>
+
+#include "common.pb.h"
+#include "broker.pb.h"
+
+namespace mq {
+class BrokerManager {
+public:
+    Status RegisterBroker(std::string ip, int32_t port);
+    Status UnRegisterBroker(std::string ip, int32_t port);
+private:
+    std::map<int32_t, BrokerInfo*> _brokers;
+};
+}
+
+#endif
